e90632ede20339b6a0edd90dcff2d01c9b8a5f32
GeogeBM <jbetma@gmail.com>
2022-03-30 18:33:43 -0600

    commit

---
+++ src/app/services/producto/producto.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductoService {
+
+  private BASE_URL = 'https://localhost:44388/api/';
+  constructor(
+    private http:HttpClient
+  ) { }
+}
